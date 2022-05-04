d428bdc596875708861d16ab9acc71811e83a881
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-05-04 09:12:12 +0200

    added vite entry point for print.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
 import copy from "rollup-plugin-copy"
 import postcssNesting from "postcss-nesting"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                print: resolve(__dirname, "print.html"),
+            },
+        },
+    },
     css: {
         postcss: {
             plugins: [postcssNesting()],
